b98a713692c0b4dafa638785653f8d8b0c954850
Victor <victor3790@gmail.com>
2020-12-19 11:48:32 -0600

    test 3

--- notifications.php
+++ notifications.php
@@ -6,9 +6,16 @@ require __DIR__ .  '/vendor/autoload.php';
 
 MercadoPago\SDK::setAccessToken("APP_USR-1159009372558727-072921-8d0b9980c7494985a5abd19fbe921a3d-617633181");
 
-/*switch($_POST["type"]) {
+switch($_POST["type"]) {
     case "payment":
         $payment = MercadoPago\Payment.find_by_id($_POST["id"]);
+        $fichero = './payments.php';
+// Abre el fichero para obtener el contenido existente
+$actual = file_get_contents($fichero);
+// Añade una nueva persona al fichero
+$actual .= "John Smith\n";
+// Escribe el contenido al fichero
+file_put_contents($fichero, $actual);
         break;
     case "plan":
         $plan = MercadoPago\Plan.find_by_id($_POST["id"]);
@@ -19,14 +26,6 @@ MercadoPago\SDK::setAccessToken("APP_USR-1159009372558727-072921-8d0b9980c749498
     case "invoice":
         $plan = MercadoPago\Invoice.find_by_id($_POST["id"]);
         break;
-}*/
-
-$fichero = './payments.php';
-// Abre el fichero para obtener el contenido existente
-$actual = file_get_contents($fichero);
-// Añade una nueva persona al fichero
-$actual .= "John Smith\n";
-// Escribe el contenido al fichero
-file_put_contents($fichero, $actual);
+}
 
 ?>
