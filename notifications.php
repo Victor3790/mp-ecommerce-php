bca1de79efbd5960beb746add42d2083f462169e
Victor <victor3790@gmail.com>
2020-12-19 11:30:12 -0600

    New files added for testing live

---
+++ notifications.php
@@ -0,0 +1,28 @@
+<?php
+
+require __DIR__ .  '/vendor/autoload.php';
+
+$dumb = 1+1;
+
+MercadoPago\SDK::setAccessToken("APP_USR-1159009372558727-072921-8d0b9980c7494985a5abd19fbe921a3d-617633181");
+
+switch($_POST["type"]) {
+    case "payment":
+        $payment = MercadoPago\Payment.find_by_id($_POST["id"]);
+        break;
+    case "plan":
+        $plan = MercadoPago\Plan.find_by_id($_POST["id"]);
+        break;
+    case "subscription":
+        $plan = MercadoPago\Subscription.find_by_id($_POST["id"]);
+        break;
+    case "invoice":
+        $plan = MercadoPago\Invoice.find_by_id($_POST["id"]);
+        break;
+}
+
+//http_response_code(200);
+
+?>
+
+<h1>Hey</h1>
