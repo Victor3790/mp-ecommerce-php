244f5b75780654a9d75c8856d3f1265fe0d4eb6c
Victor <victor3790@gmail.com>
2020-12-20 22:44:44 -0600

    test 40

--- detail.php
+++ detail.php
@@ -3,7 +3,7 @@
     require __DIR__ .  '/vendor/autoload.php';
 
     // Agrega credenciales
-    MercadoPago\SDK::setAccessToken('APP_USR-1159009372558727-072921-8d0b9980c7494985a5abd19fbe921a3d-617633181-00');
+    MercadoPago\SDK::setAccessToken('APP_USR-1159009372558727-072921-8d0b9980c7494985a5abd19fbe921a3d-617633181');
     MercadoPago\SDK::setIntegratorId("dev_24c65fb163bf11ea96500242ac130004");
 
     $home = $_SERVER['HTTP_REFERER'];
