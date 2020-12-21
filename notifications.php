86819fd43b3e96c28b620be86e1e4ffc04891b5d
Victor <victor3790@gmail.com>
2020-12-20 20:27:32 -0600

    test 16

--- notifications.php
+++ notifications.php
@@ -8,6 +8,8 @@ $actual .= "John Smith\n";
 // Escribe el contenido al fichero
 file_put_contents($fichero, $actual);
 
+echo 'hola amigos';
+
 require __DIR__ .  '/vendor/autoload.php';
 
 MercadoPago\SDK::setAccessToken('APP_USR-1159009372558727-072921-8d0b9980c7494985a5abd19fbe921a3d-617633181');
