240043bab890af5098f387eab3d4818bb71bf360
Victor <victor3790@gmail.com>
2020-12-21 09:16:15 -0600

    test 47

--- notifications.php
+++ notifications.php
@@ -4,9 +4,11 @@ require __DIR__ .  '/vendor/autoload.php';
 
 MercadoPago\SDK::setAccessToken('APP_USR-1159009372558727-072921-8d0b9980c7494985a5abd19fbe921a3d-617633181');
 
+$json = file_get_contents('php://input');
+
 $fichero = 'payments.txt';
 $actual = file_get_contents($fichero);
-$actual .= var_dump($_POST);
+$actual .= $json;
 file_put_contents($fichero, $actual);
 
 ?>
