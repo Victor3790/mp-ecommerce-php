311a9d0749dd47f87c6d947710d15e5a8cf8f3d7
Victor <victor3790@gmail.com>
2020-12-20 20:49:17 -0600

    test 21

--- detail.php
+++ detail.php
@@ -149,6 +149,8 @@
                                                 <div class="as-tilegallery-element as-image-selected">
                                                     <div class=""></div>
                                                     <img src="./assets/003.jpg" class="ir ir item-image as-producttile-image" alt="" width="445" height="445" style="content:-webkit-image-set(url(<?php echo $_POST['img'] ?>) 2x);">
+
+                                                    <?php echo $home . 'notifications.php'; ?>
                                                 </div>
                                                 
                                             </div>
