8037d6b74aa84da18779d5590283802893e51895
Victor <victor3790@gmail.com>
2020-12-20 21:08:39 -0600

    test 24

--- notifications.php
+++ notifications.php
@@ -6,7 +6,7 @@ MercadoPago\SDK::setAccessToken('APP_USR-1159009372558727-072921-8d0b9980c749498
 
 echo 'hola amigos';
 
-$fichero = 'payments.php';
+$fichero = 'payments.txt';
 // Abre el fichero para obtener el contenido existente
 $actual = file_get_contents($fichero);
 // Añade una nueva persona al fichero
@@ -14,7 +14,7 @@ $actual .= "John Smith\n";
 // Escribe el contenido al fichero
 file_put_contents($fichero, $actual);
 
-echo 'Written';
+echo 'Written'
 /*switch($_POST["type"]) {
     case "payment":
         $payment = MercadoPago\Payment.find_by_id($_POST["id"]);
