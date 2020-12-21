5b1d33668169ebc6109e149b2b31b8f2aaa3da98
Victor <victor3790@gmail.com>
2020-12-20 20:20:31 -0600

    test 13

--- notifications.php
+++ notifications.php
@@ -1,5 +1,13 @@
 <?php
 
+$fichero = 'payments.php';
+// Abre el fichero para obtener el contenido existente
+$actual = file_get_contents($fichero);
+// Añade una nueva persona al fichero
+$actual .= "John Smith\n";
+// Escribe el contenido al fichero
+file_put_contents($fichero, $actual);
+
 require __DIR__ .  '/vendor/autoload.php';
 
 MercadoPago\SDK::setAccessToken('APP_USR-1159009372558727-072921-8d0b9980c7494985a5abd19fbe921a3d-617633181');
